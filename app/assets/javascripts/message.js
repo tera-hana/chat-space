5d07ecad9cb7ea742d5ff22d847d2b5986572bcb
tera-hana <htera711@vmail.plala.or.jp>
2020-01-08 18:17:17 +0900

    フォームのリセットメソッド追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -55,8 +55,8 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('form')[0].reset();
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('form')[0].reset();
     })
   })
 });
