f4e500cacd89bbb51dd286bc5bf513e436c28eef
tera-hana <htera711@vmail.plala.or.jp>
2020-01-08 16:32:02 +0900

    json-HTML形成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,46 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="message__upper-info">
+           <div class="message__upper-info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper-info__date">
+             ${message.created_at}
+           </div>
+         </div>
+           <p class="message__text">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="message__upper-info">
+           <div class="message__upper-info__talker">
+             ${message.user_name}
+           </div>
+           <div class="message__upper-info__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="message__text">
+           <p class="message__text">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
 $(function(){
   $('#new_message').on('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -11,5 +51,9 @@ $(function(){
       processData: false,
       contentType: false
     })
-  });
+    .done(function(data){
+      var html = buildHTML(data);
+    })
+  })
+});
 });
