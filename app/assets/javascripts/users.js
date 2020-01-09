d1d37f8e06cf2d7aa11b55aaf995df81501fe151
tera-hana <htera711@vmail.plala.or.jp>
2020-01-09 20:56:14 +0900

    5.非同期通信の結果を得て、HTMLを作成する

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,17 +1,46 @@
 $(function() {
-  $("#user-search-field").on("keyup", function(e) {
-    var input = $("#user-search-field").val();
-    $.ajax({
-      type: "GET",
-      url: "/users",
-      data: { keyword: input },
-      dataType: "json"
-    })
-    .done(function(ussers) {
-      console.log("成功です");
-    })
-    .fail(function() {
-      console.log("失敗です");
+  function  addUser(user) {
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザー名</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+              </div>
+              `;
+    $("#user-search-result").append(html);
+  }
+
+  function  addNoUser() {
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザーが見つかりません</p>
+              </div>
+              `;
+    $("#user-search-result").append(html);             
+  }
+
+    $("#user-search-field").on("keyup", function(e) {
+      var input = $("#user-search-field").val();
+      $.ajax({
+        type: "GET",
+        url: "/users",
+        data: { keyword: input },
+        dataType: "json"
+      })
+      .done(function(users) {
+        $("#user-search-result").empty();
+
+        if (users.length !== 0) {
+          users,forEach(function(user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+      .fail(function() {
+
     });
   });
 });
