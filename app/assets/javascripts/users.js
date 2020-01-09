b9640aea6a0f21004fcb8cf0fe2627b0851349a5
tera-hana <htera711@vmail.plala.or.jp>
2020-01-09 19:47:16 +0900

    4.イベント時に非同期通信可能にする

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,17 @@
 $(function() {
   $("#user-search-field").on("keyup", function(e) {
     var input = $("#user-search-field").val();
-    console.log(input);
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+    .done(function(ussers) {
+      console.log("成功です");
+    })
+    .fail(function() {
+      console.log("失敗です");
+    });
   });
 });
