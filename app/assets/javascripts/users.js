958f3c5738348894abced78369710c1dfc789230
tera-hana <htera711@vmail.plala.or.jp>
2020-01-11 01:19:57 +0900

    5.非同期通信の結果を得て、HTMLを作成する(リトライ)
    正常に動作しなかったためコードを修正した。

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,36 +1,35 @@
 $(function() {
-  function  addUser(user) {
-    var html = `
-              <div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">ユーザー名</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
-              </div>
-              `;
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
     $("#user-search-result").append(html);
   }
 
-  function  addNoUser() {
-    var html = `
-              <div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">ユーザーが見つかりません</p>
-              </div>
-              `;
-    $("#user-search-result").append(html);             
+  function addNoUser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
   }
-
-    $("#user-search-field").on("keyup", function(e) {
-      var input = $("#user-search-field").val();
-      $.ajax({
-        type: "GET",
-        url: "/users",
-        data: { keyword: input },
-        dataType: "json"
-      })
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
       .done(function(users) {
         $("#user-search-result").empty();
 
         if (users.length !== 0) {
-          users,forEach(function(user) {
+          users.forEach(function(user) {
             addUser(user);
           });
         } else if (input.length == 0) {
@@ -42,5 +41,5 @@ $(function() {
       .fail(function() {
         alert("ユーザー検索に失敗しました");
     });
-  });
+});
 });
