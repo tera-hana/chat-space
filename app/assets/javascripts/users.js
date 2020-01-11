c2052d080bcd898954736928dfe66d6923c5c94e
tera-hana <htera711@vmail.plala.or.jp>
2020-01-12 01:05:03 +0900

    1.追加ボタンが押された時にイベント発火

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -18,6 +18,20 @@ $(function() {
     `;
     $("#user-search-result").append(html);
   }
+  // 追加ボタンを押されたユーザ名を検索結果から消す
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  // グループに追加するメンバーのidを送信する
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
   // テキストフィールド入力時にイベント発火
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
@@ -46,4 +60,20 @@ $(function() {
         alert("ユーザー検索に失敗しました");
     });
 });
+// 追加ボタンが押された時にイベント発火
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log('イベント発火')
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
+  });
 });
