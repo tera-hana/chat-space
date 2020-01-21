e6cb319b1b40f5423d90a8619a867a93f89e779f
tera-hana <htera711@vmail.plala.or.jp>
2020-01-21 12:30:36 +0900

    バケット再作成のため修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chat-space-terahana-image'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-terahana-image'
+  config.fog_directory  = 'chatspaceimagetera'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspaceimagetera'
 end
